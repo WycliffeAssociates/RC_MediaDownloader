fd2da002358ba8f07b40e2f1e4c18e07bebebe52
Hoang Anh <hoanganhtran1998@gmail.com>
2020-08-27 08:44:52 -0400

    Update Main.kt

--- src/main/kotlin/org/wycliffeassociates/resourcecontainer/media/Main.kt
+++ src/main/kotlin/org/wycliffeassociates/resourcecontainer/media/Main.kt
@@ -3,10 +3,9 @@ package org.wycliffeassociates.resourcecontainer.media
 import java.io.File
 
 fun main() {
-    val rcFile = File("E:/miscs/rc/pentateuch.zip")
+    val rcFile = File("S:/Misc/en_ulb_pentateuch")
     val parameter =  MediaUrlParameter(
         projectId = "gen",
         mediaTypes = listOf(MediaType.WAV)
     )
     MediaDownloader(rcFile).download(parameter)
-}
