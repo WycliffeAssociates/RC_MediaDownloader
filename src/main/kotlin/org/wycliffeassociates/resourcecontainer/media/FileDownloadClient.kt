75a1d9c6fee5c434da4c630810d58cdb1cef4c9a
Hoang Anh <hoanganhtran1998@gmail.com>
2020-08-28 16:25:18 -0400

    add retrofit dependency

--- src/main/kotlin/org/wycliffeassociates/resourcecontainer/media/FileDownloadClient.kt
+++ src/main/kotlin/org/wycliffeassociates/resourcecontainer/media/FileDownloadClient.kt
@@ -1,7 +1,13 @@
 package org.wycliffeassociates.resourcecontainer.media
 
-import java.io.File
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Streaming
+import retrofit2.http.Url
 
 interface FileDownloadClient {
-    
+    @Streaming
+    @GET
+    fun downloadFileStream(@Url url: String): Call<ResponseBody>
 }
