3dbff39268600207f79ea0b17abfbab53f3620b6
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-10-15 15:23:38 -0400

    Download a specific chapter (#17)
    * add single chapter download option
    
    * lint
    
    * add verify method call

--- lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/ChapterMediaDownloader.kt
+++ lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/ChapterMediaDownloader.kt
@@ -15,11 +15,37 @@ class ChapterMediaDownloader(
 ) : RCMediaDownloader(urlParams, downloadClient) {
 
     override fun downloadMedia(url: String, rc: ResourceContainer): String {
+        return if (urlParams.chapter != null) {
+            downloadSingleChapter(urlParams.chapter, url, rc)
+        } else {
+            downloadAllChapters(url, rc)
+        }
+    }
+
+    private fun downloadSingleChapter(chapterNumber: Int, templateUrl: String, rc: ResourceContainer): String {
+        val contentDir = createTempDir()
+        val formattedUrl = templateUrl.replace("{chapter}", chapterNumber.toString())
+
+        val downloadedFile = downloadClient.downloadFromUrl(formattedUrl, contentDir)
+        if (downloadedFile != null) {
+            val pathInRC = templatePathInRC(downloadedFile.name, MediaDivision.CHAPTER)
+            rc.addFileToContainer(downloadedFile, pathInRC)
+        }
+
+        contentDir.deleteRecursively() // delete temp dir after downloaded
+
+        return templatePathInRC(
+            File(templateUrl).name,
+            MediaDivision.CHAPTER
+        )
+    }
+
+    private fun downloadAllChapters(templateUrl: String, rc: ResourceContainer): String {
         val possibleChapterRange = 200
         val chapterUrlList = mutableListOf<String>()
 
         for (chapterNumber in 1..possibleChapterRange) {
-            val chapterUrl = url.replace("{chapter}", chapterNumber.toString())
+            val chapterUrl = templateUrl.replace("{chapter}", chapterNumber.toString())
             chapterUrlList.add(chapterUrl)
         }
 
@@ -31,17 +57,18 @@ class ChapterMediaDownloader(
                 launch {
                     val downloadFile = downloadClient.downloadFromUrl(url, contentDir)
                     if (downloadFile != null) {
-                        val pathInRC = "$MEDIA_DIR/${urlParams.projectId}/chapters/${File(url).name}"
+                        val pathInRC = templatePathInRC(File(url).name, MediaDivision.CHAPTER)
                         filesToRCMap[pathInRC] = downloadFile
                     }
                 }
             }
         }
+
         rc.addFilesToContainer(filesToRCMap)
         contentDir.deleteRecursively() // delete temp dir after downloaded
 
         return templatePathInRC(
-            File(url).name,
+            File(templateUrl).name,
             MediaDivision.CHAPTER
         )
     }
--- lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/data/MediaUrlParameter.kt
+++ lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/data/MediaUrlParameter.kt
@@ -3,5 +3,6 @@ package org.wycliffeassociates.rcmediadownloader.data
 data class MediaUrlParameter(
     val projectId: String,
     val mediaDivision: MediaDivision,
-    val mediaTypes: List<MediaType>
+    val mediaTypes: List<MediaType>,
+    val chapter: Int? = null
 )
