fcb78d44827b27a695ae79135034362f9fe61571
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-11-19 09:44:37 -0500

    Add okhttp client for download (#25)
    * add okhttp client for download (alternative for retrofit)
    
    * version
    
    * linter
    
    * init client as class property

---
+++ lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/io/OkHttpDownloadClient.kt
@@ -0,0 +1,46 @@
+package org.wycliffeassociates.rcmediadownloader.io
+
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.ResponseBody
+import org.slf4j.LoggerFactory
+
+class OkHttpDownloadClient : IDownloadClient {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val client = OkHttpClient()
+
+    override fun downloadFromUrl(url: String, outputDir: File): File? {
+        val urlFile = File(url)
+        val outputFile = outputDir.resolve(urlFile.name)
+
+        val request: Request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.isSuccessful) {
+                if (response.body == null) {
+                    logger.error("No response body found. ${response.message}")
+                } else {
+                    writeTempDownload(response.body!!, outputFile)
+                }
+            }
+        }
+
+        return if (outputFile.isFile) outputFile else null
+    }
+
+    private fun writeTempDownload(body: ResponseBody, outputFile: File): File {
+        BufferedInputStream(body.byteStream()).use { inputStream ->
+            val bytes = inputStream.readBytes()
+
+            FileOutputStream(outputFile).buffered().use { outputStream ->
+                outputStream.write(bytes)
+            }
+        }
+        return outputFile
+    }
+}
