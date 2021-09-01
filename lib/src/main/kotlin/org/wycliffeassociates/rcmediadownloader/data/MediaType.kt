f88335565c8ac67cdda4bcb897cc66c2e2f6ec1e
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-09-01 15:57:32 -0400

    add cue type

--- lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/data/MediaType.kt
+++ lib/src/main/kotlin/org/wycliffeassociates/rcmediadownloader/data/MediaType.kt
@@ -3,7 +3,10 @@ package org.wycliffeassociates.rcmediadownloader.data
 enum class MediaType {
     WAV,
     PNG,
-    MP3;
+    MP3,
+    CUE;
+
+    override fun toString() = this.name.toLowerCase()
 
     companion object {
         fun get(type: String) = values().firstOrNull { it.name == type.toUpperCase() }
